b1b03760c039114400bcf6d4378aefc2703789b7
Mohammed Achraf LEBRAZI <lebrazi.med.achraf@gmail.com>
2019-05-21 13:08:14 +0200

    Created main activity initial UI

--- app/src/main/java/org/uhworks/smack/MainActivity.kt
+++ app/src/main/java/org/uhworks/smack/MainActivity.kt
@@ -11,6 +11,7 @@ import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.view.Menu
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -40,5 +41,17 @@ class MainActivity : AppCompatActivity() {
         } else {
             super.onBackPressed()
         }
+    }
+
+    fun loginBtnNavHeaderClicked(view: View) {
+
+    }
+
+    fun addChannelClicked(view: View) {
+
+    }
+
+    fun sendMessageBtnClicked(view: View) {
+
     }
 }
